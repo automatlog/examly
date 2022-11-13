ff1c61dc00979d9bffec27afd63e48411def0c31
Aman Yadav <72098281+automatlog@users.noreply.github.com>
2022-11-13 20:50:19 +0530

    Add files via upload

---
+++ js/qnaneet1.js
@@ -0,0 +1,357 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "For a p-type semiconductor, which of the following statements is true ?",
+    answer: "Holes are the majority carriers and trivalent atoms are the dopants.",
+    options: [
+      "Holes are the majority carriers and trivalent atoms are the dopants.",
+      "Holes are the majority carriers and pentavalent atoms are the dopants",
+      "Holes are the majority carriers and pentavalent atoms are the dopants",
+      "Electrons are the majority carriers and trivalent atoms are the dopants."
+    ]
+  },
+    {
+    numb: 2,
+    question: "A body weighs 200 N on the surface of the earth. How much will it weigh half way down to the centre of the earth ??",
+    answer: "100 N",
+    options: [
+      "200 N",
+      "250 N",
+      "100 N",
+      "150 N"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In which of the following processes, heat is neither absorbed nor released by a system?",
+    answer: "Adiabatic",
+    options: [
+      "Adiabatic",
+      "Isobaric",
+      "Isochoric",
+      "Isothermal"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A force F = 20 +10 y acts on a particle in y-direction, where F is in newton and y in meter.Work done by this force to move the particle from y = 0 to y = 1m is",
+    answer: "25 J",
+    options: [
+      "10 J",
+      "25 J",
+      "20 J",
+      "30 J"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Two point charges A and B, having charges +Q and -Q respectively, are placed at certain distance apart and force acting between them is F. If 25% charge of A is transferred to B, then force between the charges becomes",
+    answer: "9F/16",
+    options: [
+      "16F/9",
+      "9F/16",
+      "4F/3",
+      "f"
+    ]
+  },
+     {
+     numb: 6,
+     question: "Pick the wrong answer in the context with rainbow.",
+     answer: "An observer can see a rainbow when his front is towards the sun.",
+     options: [
+       "The order of colours is reversed in the secondary rainbow.",
+       "An observer can see a rainbow when his front is towards the sun.",
+       "Rainbow is a combined effect of dispersion refraction and reflection of sunlight.",
+       "When the light rays undergo two internal reflections in a water drop, a secondary rainbow is formed."
+     ]
+   },
+
+   {
+    numb: 7,
+    question: "Which of the following acts as a circuit protection device?",
+    answer: "Fuse",
+    options: [
+      "Inductor",
+      "Switch",
+      "Fuse",
+      "Conductor"
+    ]
+  },
+  
+  {
+    numb: 8,
+    question: "A mass m is attached to a thin wire and whirled in a vertical circle.The wire is most likely to break when:",
+    answer: "The mass is at the lowest point",
+    options: [
+      "The wire is horizontal",
+      "The mass is at the lowest point",
+      "The mass is at the highest point",
+      "Inclined at an angle of 60° from vertical"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Which colour of the light has the longest wavelength?",
+    answer: "Red",
+    options: [
+      "Blue",
+      "Green",
+      "Red",
+      "Violet"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Alpha-particle consists of",
+    answer: "2 protons and 2 neutrons only",
+    options: [
+      "2 electrons, 2 protons and 2 neutrons",
+      "2 electrons and 4 protons only",
+      "2 protons only",
+      "2 protons and 2 neutrons only"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "A copper rod of 88 cm and an aluminium rod of unknown length have their increase in length independent of increase in temperature. The length of aluminium rod is",
+    answer: "68 cm",
+    options: [
+      "6.8 cm",
+      "88 cm",
+      "113.9 cm",
+      "68 cm"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "In total internal reflection when the angle of incidence is equal to the critical angle for the pair of media in contact, what will be angle of refraction?",
+    answer: "90 degree",
+    options: [
+      "0 degree",
+      "Equal to angle of incidence",
+      "90 degree",
+      "180 degree"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Body A of mass 4m moving with speed u collides with another body B of mass 2m, at rest. The collision is head on and elastic in nature. After the collision the fraction of energy lost by the colliding body A is",
+    answer: "8/9",
+    options: [
+      "4/9",
+      "8/9",
+      "5/9",
+      "1/9"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "A coil of 800 turns effective area 0.05 m2 is kept perpendicular to a magnetic field 5 10 ´ 5 - T. When the plane of the coil is rotated by 90º around any of its co-planar axis in 0.1 s, the emf induced in the coil will be",
+    answer: "0.02 V",
+    options: [
+      "0.2 V",
+      "2 V",
+      "0.02 V",
+      "2*10 V"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Increase in temperature of a gas filled in a container would lead to",
+    answer: "Increase in its kinetic energy",
+    options: [
+      "Increase in its kinetic energy",
+      "Decrease in its pressure",
+      "Decrease in intermolecular distance",
+      "Increase in its mass"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "A disc of radius 2 m and mass 100 kg rolls on a horizontal floor. Its centre of mass has speed of 20 cm/s. How much work is needed to stop it?",
+    answer: "3 J",
+    options: [
+      "30 J",
+      "2 J",
+      "1 J",
+      "3 J"
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "A hollow metal sphere of radius R is uniformly charged.The electric field due to the sphere at a distance r from the centre",
+    answer: "zero as r increases for r < R, decreases as r increases for r > R",
+    options: [
+      "zero as r increases for r < R, decreases as r increases for r > R",
+      "zero as r increases for r < R, increases as r increases for r > R",
+      "Decreases as r increases for r < R and for r > R",
+      "Increases as r increases for r < R and for r > R"                
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "An object kept in a large room having air temperature of 25°C takes 12 minutes to cool from 80°C to 70°C.The time taken to cool for the same object from 70°C to 60°C would be nearly",
+    answer: "15 min",
+    options: [
+      "5 min",
+      "10 min",
+      "15 min",
+      "20 min"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "The distance covered by a particle undergoing SHM in one time period is (amplitude = A)",
+    answer: "4A",
+    options: [
+      "Zero",
+      "A",
+      "4A",
+      "2A"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "A tuning fork with frequency 800 Hz produces resonance in a resonance column tube with upper end open and lower end closed by water surface. Successive resonance are observed at length 9.75 cm, 31.25 cm and 52.75 cm.The speed of sound in air is",
+    answer: "344 m/s",
+    options: [
+      "500 m/s",
+      "156 m/s",
+      "344 m/s",
+      "172 m/s"
+    ]
+  },
+
+  {
+    numb: 21,
+    question: "An equi-convex lens has power P it is cut into two symmetrical halves by a plane containing the principal axis. The power of one part will be",
+    answer: "P",
+    options: [
+      "0",
+      "P/2",
+      "P/4",
+      "P"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "The work function of a photosensitive material is 4.0 eV. This longest wavelength of light that can cause photon emission from the substance is (approximately)",
+    answer: "310nm",
+    options: [
+      "31000nm",
+      "31nm",
+      "3100nm",
+      "310nm"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "Angular width of the central maxima in the Fraunhoffer diffraction for l = 6000 Å is q0. When thesame slit is illuminated by another monochromatic light, the angular width decreases by 30%. The wavelength of this light is",
+    answer: "4200 Å",
+    options: [
+      "1800 Å",
+      "4200 Å",
+      "6000 Å",
+      "420 Å"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "A cycle wheel of radius 0.5 m is rotated with constant angular velocity of 10 rad/s in a region of magnetic field of 0.1 T which is perpendicular to the plane of thewheel. The EMF generated between its centre and the rim is",
+    answer: "0.125 V",
+    options: [
+      "0.25 V",
+      "0.125 V",
+      "0.5 V",
+      "Zero"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "A circuit when connected to an AC source of 12 V gives a current of 0.2 A. The same circuit when connected to a DC source of 12 V, gives a current of 0.4 A. The circuit is",
+    answer: "series LR",
+    options: [
+      "series LR",
+      "series RC",
+      "series LCR",
+      "series LC"
+    ]
+  },
+
+  {
+    numb: 26,
+    question: "Two toroids 1 and 2 have total number of turns 200 and 100 respectively with average radii 40 cm and 20 cm respectively. If they carry same current i, the ratio of the magnetic fields along the two loops is",
+    answer: "1:1",
+    options: [
+      "4:1",
+      "1:1",
+      "2:1",
+      "1:2"
+    ]
+  },
+
+  {
+    numb: 27,
+    question: "A solid cylinder of mass 2 kg and radius 50 cm rolls up an inclined plane of angle inclination 30°. The centre of mass of cylinder has speed of 4 m/s. The distance travelled by the cylinder on the inclined surface will be :(Take g = 10 m/s2)",
+    answer: "2.4 m",
+    options: [
+      "2.0 m",
+      "1.6 m",
+      "2.4 m",
+      "2.2 m"
+    ]
+  },
+
+  {
+    numb: 28,
+    question: "A double convex lens has focal length 25 cm. The radius of curvature of one of the surfaces is doubled of the other. Find the radii, if the refractive index of the material of the lens is 1.5.",
+    answer: "18.75 cm, 37.5 cm",
+    options: [
+      "100cm, 50cm",
+      "18.75 cm, 37.5 cm",
+      "19 cm, 20 cm",
+      "120 cm, 50 cm"
+    ]
+  },
+
+  {
+    numb: 29,
+    question: "The stress-strain curves are drawn for two different materials X and Y. It is observed that the ultimate strength point and the fracture point are close to eachother for material X but are far apart for material Y. We can say that materials X and Y are likely to be (respectively)",
+    answer: "Brittle and Ductile",
+    options: [
+      "Plastic and Ductile",
+      "Ductile and Plastic",
+      "Brittle and Ductile",
+      "Brittle and Plastic"
+    ]
+  },
+
+  {
+    numb: 30,
+    question: "An object kept in a large room having air temperature of 25°C takes 12 minutes to cool from 80°C to 70°C. The time taken to cool for the same object from 70°C to 60°C would be nearly",
+    answer: "15 min",
+    options: [
+      "20 min",
+      "12 min",
+      "10 min",
+      "15 min"
+    ]
+  },
+];
---
+++ js/qnaneet2.js
@@ -0,0 +1,361 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The ratio of contributions made by the electric field and magnetic field components, to the intensity of an electromagnetic wave is (where, c = speed of electromagnetic waves)",
+    answer: "c:1",
+    options: [
+      "1:2",
+      "1:1",
+      "1:c",
+      "c:1"
+    ]
+  },
+
+  {
+    numb: 2,
+    question: "In a certain region of space with volume 0.2 cubic meter, the electric potential is found to be 5 V throughout. The magnitude of electric field in this region is",
+    answer: "Zero",
+    options: [
+      "0.5 N/C",
+      "5 N/C",
+      "1 N/C",
+      "Zero"
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "A screw gauge has least count of 0.01 mm and there are 50 divisions in its circular scale. The pitch of the screw gauge is",
+    answer: "0.5 mm",
+    options: [
+      "0.25 mm",
+      "1.0 mm",
+      "0.5 mm",
+      "0.01 mm"
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "In Young’s double slit experiment, if the separation between coherent sources is halved and the distance of the screen from the coherent sources is doubled, then the fringe width becomes",
+    answer: "Four times",
+    options: [
+      "Half",
+      "Four times",
+      "One-fourth",
+      "Double"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "Taking into account of the significant figures, what is the value of 9.99 m − 0.0099 m?",
+    answer: "9.980 m",
+    options: [
+      "9.98 m",
+      "9.980 m",
+      "9.9 m",
+      "9.9801 m"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "A capillary tube of radius r is immersed in water a water rises in it to a height h. The mass of the water in the capillary tube is 5 g. Another capillary tube of radius 2r is immersed in water. The mass of water that will rise in this tube is",
+    answer: "2.5 g",
+    options: [
+      "5.0 g",
+      "10.0 g",
+      "2.5 g",
+      "20.0 g"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "A body weighs 72 N on the surface of the earth. What is the gravitational force on it, at a height equal to half of radius of the earth?",
+    answer: "32 N",
+    options: [
+      "32 N",
+      "30 N",
+      "25 N",
+      "48 N"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Two particles of mass 5 kg and 10 kg respectively are attached to the two ends of a rigid rod of length 1 m with negligible mass. The centre of mass of the system from the 5 kg particle is nearly at a distance of",
+    answer: "67 cm",
+    options: [
+      "60 cm",
+      "67 cm",
+      "57 cm",
+      "33 cm"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "The increase in the width of the depletion region in a p-n junction diode is due to",
+    answer: "Reverse bias only",
+    options: [
+      "Reverse bias only",
+      "Both forward bias and reverse bias",
+      "Increase in forward current",
+      "Forward bias only"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Light of frequency 1.5 times the threshold frequency is incident on a photosensitive material. What will be the photoelectric current if the frequency is halved and intensity is doubled?",
+    answer: "Zero",
+    options: [
+      "Four Times",
+      "One Fourth",
+      "Zero",
+      "Doubled"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "In a guitar, two strings A and B made of same material are slightly out of tune and produce beats of frequency 6 Hz. When tension in B is slightly decreased, the beat frequency increases to 7 Hz. If the frequency of A is 530 Hz, the original frequency of B will be",
+    answer: "524 Hz",
+    options: [
+      "524 Hz",
+      "536 Hz",
+      "537 Hz",
+      "523 Hz"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "The solids which have the negative temperature coefficient of resistance are",
+    answer: "Insulators and Semiconductors",
+    options: [
+      "Insulators Only",
+      "Semiconductors Only",
+      "Insulators and Semiconductors",
+      "Metals"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "For transistor action, which of the following statements is correct?",
+    answer: "The base region must be very thin and lightly doped",
+    options: [
+      "Base, emitter and collector regions should have same size.",
+      "Both emitter junction as well as the collector junction are forward biased",
+      "The base region must be very thin and lightly doped",
+      "Base, emitter and collector regions should have same dopping concentrations"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "A body is executing simple harmonic motion with frequency n, the frequency of its potential energy is",
+    answer: "2n",
+    options: [
+      "n",
+      "2n",
+      "3n",
+      "4n"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "A spring is stretched by 5 cm by a force 10 N. The time period of the oscillations when a mass of 2 kg is suspended by it is",
+    answer: "0.628 s",
+    options: [
+      "0.0628 s",
+      "6.28 s",
+      "3.14 s",
+      "0.628 s"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "A nucleus with mass number 240 breaks into two fragments each of mass number 120, the binding energy per nucleon of unfragmented nuclei is 7.6 MeV while that of fragments is 8.5 MeV. The",
+    answer: "216 MeV",
+    options: [
+      "0.9 MeV",
+      "9.4 MeV",
+      "804 MeV",
+      "216 MeV"
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "A screw gauge gives the following readings when used to measure the diameter of a wire Main scale reading : 0 mm Circular scale reading : 52 divisions Given that, 1 mm on main scale corresponds to 100 divisions on the circular scale. The diameter of the wire from the above data is",
+    answer: "0.052 cm",
+    options: [
+      "0.52 cm",
+      "0.026 cm",
+      "0.26 cm",
+      "0.052 cm"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "The escape velocity from the Earth’s surface is v. The escape velocity from the surface of another planet having a radius, four times that of Earth and same mass density is",
+    answer: "4 v",
+    options: [
+      "v",
+      "3 v",
+      "2 v",
+      "4 v"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "In a potentiometer circuit, a cell of emf 1.5 V gives balance point at 36 cm length of wire. If another cell of emf 2.5 V replaces the first cell, then at what length of the wire, the balance point occurs?",
+    answer: "60 cm",
+    options: [
+      "60 cm",
+      "21.6 cm",
+      "64 cm",
+      "62 cm"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "The velocity of a small ball of mass M and density d when dropped in a container filled with glycerine becomes constant after some time. If the density of glycerine is d/2, then the viscous force acting on the ball will be",
+    answer: "Mg/2",
+    options: [
+      "Mg",
+      "Mg/2",
+      "3/2*Mg",
+      "2 Mg"
+    ]
+  },
+
+  {
+    numb: 21,
+    question: "A lens of large focal length and large aperture is best suited as an objective of an astronomical telescope, since",
+    answer: "All of the above",
+    options: [
+      "a large aperture contributes to the quality and visibility of the images",
+      "a large area of the objective ensures better light gathering power",
+      "a large aperture provides a better resolution",
+      "All of the above"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "The electron concentration in an n-type semiconductor is the same as hole concentration in a p-type semiconductor. An external field (electric) is applied across each of them. Compare the currents in them.",
+    answer: "Current in n-type> Current in p-type",
+    options: [
+      "Current in n-type = Current in p-type",
+      "Current in p-type > Current in n-type",
+      "Current in n-type> Current in p-type",
+      "No current will flow in p-type. current will only flow in n-type"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "Polar molecules are the molecules",
+    answer: "having a permanent electric dipole moment",
+    options: [
+      "having zero dipole moment",
+      "acquire a dipole moment only in the presence of electric field due to displacement of charges.",
+      "acquire a dipole moment only when magnetic field is absent",
+      "having a permanent electric dipole moment"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Water falls from a height of 60 m at the rate of 15 kg/s to operate a turbine. The losses due to frictional force are 10% of the input energy. How much power is generated by the turbine?",
+    answer: "8.1 Kw",
+    options: [
+      "10.2 Kw",
+      "8.1 Kw",
+      "7.0 Kw",
+      "12.3 Kw"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "A ball of mass 0.15 kg is dropped from a height 10 m, strikes the ground and rebounds to the same height. The magnitude of impulse imparted to the ball is nearly",
+    answer: "4.2 kg-m/s",
+    options: [
+      "0",
+      "4.2 kg-m/s",
+      "2.1 kg-m/s",
+      "1.4 kg-m/s"
+    ]
+  },
+
+  {
+    numb: 26,
+    question: "Twenty seven drops of same size are charged at 220 V each. They combine to form a bigger drop. Calculate the potential of the bigger drop",
+    answer: "1980 V",
+    options: [
+      "1320 V",
+      "1980 V",
+      "660 V",
+      "440 V"
+    ]
+  },
+
+  {
+    numb: 27,
+    question: "A series L-C-R circuit containing 5.0 H inductor, 80 μF capacitor and 40 W resistor is connected to 230 V variable frequency AC source. The angular frequencies of the source at which power transferred to the circuit is half the power at the resonant angular frequency are likely to be",
+    answer: "46 rad/s and 54 rad/s",
+    options: [
+      "25 rad/s and 75 rad/s",
+      "50 rad/s and 25 rad/s",
+      "46 rad/s and 54 rad/s",
+      "42 rad/s and 58 rad/s"
+    ]
+  },
+
+  {
+    numb: 28,
+    question: "A step down transformer connected to an AC mains supply of 220 V is made to operate at 11 V, 44 W lamp. Ignoring power losses in the transformer, what is the current in the primary circuit ?",
+    answer: "0.2 A",
+    options: [
+      "2 A",
+      "0.2 A",
+      "4 A",
+      "0.4 A"
+    ]
+  },
+
+  {
+    numb: 29,
+    question: "The effective resistance of a parallel connection that consists of four wires of equal length, equal area of cross-section and same material is 0.25 W. What will be the effective resistance if they are connected in series?",
+    answer: "4 W",
+    options: [
+      "4 W",
+      "2 W",
+      "0.25 W",
+      "1 W"
+    ]
+  },
+
+  {
+    numb: 30,
+    question: "A convex lens A of focal length 20 cm and a concave lens B of focal length 5 cm are kept along the same axis with a distance d between them. If a parallel beam of light falling on A leaves B as a parallel beam, then the distance d (in cm) will be",
+    answer: "15",
+    options: [
+      "30",
+      "15",
+      "20",
+      "25"
+    ]
+  },
+];
